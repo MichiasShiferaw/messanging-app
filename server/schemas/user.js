24a3c4bc647ff85ae202b2ef86c5b98fd474a30f
Michias Shiferaw <mshif073@uottawa.ca>
2022-09-05 12:26:05 -0400

    Imported Sanity

---
+++ server/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  field: [
+    {
+      name: "userName",
+      title: "UserName",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string",
+    },
+  ],
+};
